fbc836a969642c7e572d422ec875507e5f7b2da7
Jean-Martin Archer <jm@jmartin.ca>
2025-02-17 00:05:22 -0800

    fix: handle args correctly

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,29 @@
 'use strict';
 
 import * as vscode from 'vscode';
-import * as child from 'child_process'
-import * as path from 'path'
+import * as child from 'child_process';
+import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('open-with.open', (args: string[]) => {
-    if (args.length != 1) {
-      vscode.window.setStatusBarMessage("Invalid format.");
+  let disposable = vscode.commands.registerCommand('open-with.open', (command: string) => {
+    if (!command) {
+      vscode.window.setStatusBarMessage("No command specified.");
       return;
     }
     const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      vscode.window.setStatusBarMessage("No active editor.");
+      return;
+    }
     const position = editor.selection.active;
-    const cmd = args[0]
+    const cmd = command
       .replace("{filename}", editor.document.fileName)
       .replace("{dir}", path.dirname(editor.document.fileName))
       .replace("{line}", `${position.line + 1}`)
-      .replace("{column}", `${position.character + 1}`)
+      .replace("{column}", `${position.character + 1}`);
 
     vscode.window.setStatusBarMessage(`Opening: ${cmd}`);
-    child.exec(cmd)
+    child.exec(cmd);
   });
 
   context.subscriptions.push(disposable);
