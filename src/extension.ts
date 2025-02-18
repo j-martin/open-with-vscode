614d823f38aa22ca697ac49bf31ed9229d881032
Jean-Martin Archer <jm@jmartin.ca>
2025-02-17 22:46:55 -0800

    fix: extension setup
    A base command (or contribution) is required.

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,55 @@
-'use strict';
+"use strict";
 
-import * as vscode from 'vscode';
-import * as child from 'child_process';
-import * as path from 'path';
+import * as vscode from "vscode";
+import * as child from "child_process";
+import * as path from "path";
+
+const commands: { [key: string]: string } = {
+  "Emacs (Client)": "emacsclient +{line}:{column} '{filename}'",
+  Sublime: "subl '{filename}:{line}:{column}'",
+};
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand('open-with.open', (command: string) => {
-    if (!command) {
-      vscode.window.setStatusBarMessage("No command specified.");
-      return;
-    }
-    const editor = vscode.window.activeTextEditor;
-    if (!editor) {
-      vscode.window.setStatusBarMessage("No active editor.");
-      return;
-    }
-    const position = editor.selection.active;
-    const cmd = command
-      .replace("{filename}", editor.document.fileName)
-      .replace("{dir}", path.dirname(editor.document.fileName))
-      .replace("{line}", `${position.line + 1}`)
-      .replace("{column}", `${position.character + 1}`);
+  let disposable = vscode.commands.registerCommand(
+    "open-with.open",
+    async (command: string) => {
+      if (!command) {
+        let i = 0;
+        const result = await vscode.window.showQuickPick(
+          Object.keys(commands),
+          {
+            placeHolder: "one, two or three",
+            onDidSelectItem: (item) =>
+              vscode.window.showInformationMessage(`Focus ${++i}: ${item}`),
+          }
+        );
+        if (!result) {
+          return;
+        }
+        command = commands[result];
+      }
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        vscode.window.setStatusBarMessage("No active editor.");
+        return;
+      }
+      const position = editor.selection.active;
+      const cmd = command
+        .replace("{filename}", editor.document.fileName)
+        .replace("{dir}", path.dirname(editor.document.fileName))
+        .replace("{line}", `${position.line + 1}`)
+        .replace("{column}", `${position.character + 1}`);
 
-    vscode.window.setStatusBarMessage(`Opening: ${cmd}`);
-    child.exec(cmd);
-  });
+      vscode.window.setStatusBarMessage(`Launching: ${cmd}`);
+      try {
+        child.exec(cmd);
+      } catch (error) {
+        vscode.window.setStatusBarMessage(`Error running ${cmd}: ${error}`);
+      }
+    }
+  );
 
   context.subscriptions.push(disposable);
 }
 
-export function deactivate() {
-}
+export function deactivate() {}
